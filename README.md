7f0dc387d133886f6fd2a724af81b5d1aa9c9dac
nBarcot20 <muhammad.barcot@gmail.com>
2020-03-23 18:26:48 +0600

    First Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# 100sectionClass8
---
+++ assets/js/custom.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+	
+	$('.owl-carousel').owlCarousel({
+	  items: 4,
+	  autoplay: false,
+	  loop: true,
+	  margin: 30,
+	  nav: true,
+	  navText:["<i class='fas fa-long-arrow-alt-left'></i>", "<i class='fas fa-long-arrow-alt-right'></i>"]
+	 });
+	 
+});
